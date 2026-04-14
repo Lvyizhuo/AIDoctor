7663f488156edb1301a627ceda5630fc1e9b5a9b
“Lvyizhuo” <lvonezhuo@163.com>
2026-04-14 21:43:24 +0800

    2026-04-14-21:42：跑通第一次测试，环境配置文件

--- llm/model-test.py
+++ llm/model-test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380978dc27d702ae77d318166339c0a2b6dab35635b8468356e76b88f3812106
-size 1314
+oid sha256:3bfc8e8778e6094e6b83a33f21c6e15aa9c4e89b8a783d84ad6e87ae0742db15
+size 383
