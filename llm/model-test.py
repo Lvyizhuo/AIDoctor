1534bc48dc837c78ba6ba41e43466a3a2a1e6290
Lvyizhuo <lvonezhuo@163.com>
2026-04-15 13:37:52 +0800

    2026-04-15-13:37：增加服务器训练配置文件+环境修复

--- llm/model-test.py
+++ llm/model-test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bfc8e8778e6094e6b83a33f21c6e15aa9c4e89b8a783d84ad6e87ae0742db15
-size 383
+oid sha256:c65171fd8f5b82c53499e5e41e52c4ced9734fd928710d23fe0c7616c8a2728b
+size 456
