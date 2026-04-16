8f1258d7df6e175005d1c7613d424f3bde173443
Lvyizhuo <lvonezhuo@163.com>
2026-04-16 10:16:19 +0800

    2026-04-16-10:10：第一次训练调试通过，训练了一半被楼下老头kill掉了0.0。其它信息如下： 1、原本采用（8*2）的有效batch会爆显存！！！采用（4*2）的有效batch不会爆显存（1、3数据集暂时没出现爆显存的情况，如果有，调到（1*8）！！！）； 2、nohup命令在进行双卡DDP训练的时，如果断开SSH,就会杀死进程。建议使用tmux训练； 3、依赖冲突：当前版本的transformer版本过高，不支持中断训练

--- llm/model-test.py
+++ llm/model-test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c65171fd8f5b82c53499e5e41e52c4ced9734fd928710d23fe0c7616c8a2728b
+oid sha256:c2689ce49f5c486cef4f16dcbad2bb880f10eca2edb6c11194165e3a4f6a3d02
 size 456
---
+++ llm/output/v1-20260415-192518/checkpoint-2000/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5b564062aaadd8c2d42bd838792434156bf65e1872b46fd79530ee1160b8634
+size 5262
